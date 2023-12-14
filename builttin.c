7010aceb3beb5d566ae24d74aa495b328a2eec4d
root <johnvent.otech@gmail.com>
2023-12-14 02:06:47 -0800

    flie update

--- builttin.c
+++ builttin.c
@@ -1,146 +1,115 @@
 #include "shell.h"
-/**
- *  _exit - exits the shell
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
- * @c: Count
- * Return: Void
- */
-void  exit_bul(char **cmd, char *input, char **argv, int c)
-{
-	int stat, r = 0;
-
-	if (cmd[1] == NULL)
-	{
-		free(input);
-		free(cmd);
-		exit(EXIT_SUCCESS);
-	}
-	while (cmd[1][r])
-	{
-		if (_isalpha(cmd[1][r++]) != 0)
-		{
-			_prerror(argv, c, cmd);
-			break;
-		}
-		else
-		{
-			stat = _atoi(cmd[1]);
-			free(input);
-			free(cmd);
-			exit(stat);
-		}
-	}
-}
-
 
 /**
- * _change_dir - Change Dirctorie
- * Return: 0
+ i* _myhisto - disp the history list, one command by line, preceded
+ *              with line numbers, starting at 0.
+ * @info: contain potential arguments. Used to maintain
+ *        constant function prototype.
+ *  Return: void
  */
-int change_dir(char **cmd, __attribute__((unused))int er)
+int _myhisto(info_x *info)
 {
-	int t = -1;
-	char cwd[PATH_MAX];
-
-	if (cmd[1] == NULL)
-		t = chdir(getenv("HOME"));
-	else if (_strcmp(cmd[1], "-") == 0)
-	{
-		t = chdir(getenv("OLDPWD"));
-	}
-	else
-		t = chdir(cmd[1]);
-
-	if (t == -1)
-	{
-		perror("hsh");
-		return (-1);
-	}
-	else if (t != -1)
-	{
-		getcwd(cwd, sizeof(cwd));
-		setenv("OLDPWD", getenv("PWD"), 1);
-		setenv("PWD", cwd, 1);
-	}
+	print_list(info->history);
 	return (0);
 }
-/**
- * _dis_env - display env
- * Return:0
- */
-int dis_env(__attribute__((unused)) char **cmd, __attribute__((unused)) int er)
-{
-size_t r;
-	int j;
 
-	for (r = 0; environ[r] != NULL; r++)
-	{
-		j = _strlen(environ[r]);
-		write(1, environ[r], j);
-		write(STDOUT_FILENO, "\n", 1);
-	}
-	return (0);
+/**
+ * unset_alias - alias to string
+ * @info: parameter structure
+ * @str: string alias
+ *
+ * Return: 0 on success, 1 on error
+ */
+int unset_alias(info_x *info, char *str)
+{
+	char *q, d;
+	int rat;
+
+	q = _strchr(str, '=');
+	if (!q)
+		return (1);
+	d = *q;
+	*q = 0;
+	rat = delete_node_index(&(info->alias),
+		get_node_index(info->alias, node_starts_with(info->alias, str, -1)));
+	*q = d;
+	return (rat);
 }
-/**
- * display_help - help
- * @cmd: command
- * @er: status
- * Return: 0
- */
-int display_help(char **cmd, __attribute__((unused))int er)
-{
-	int doc, w, d = 1;
-	char z;
 
-	doc = open(cmd[1], O_RDONLY);
-	if (doc < 0)
+/**
+ * set_alias - set alias to string
+ * @info: parameter structure
+ * @stre: string alias
+ *
+ * Return: 0 on success, 1 on error
+ */
+int set_alias(info_x *info, char *str)
+{
+	char *q;
+
+	q = _strchr(str, '=');
+	if (!q)
+		return (1);
+	if (!*++q)
+		return (unset_alias(info, str));
+
+	unset_alias(info, str);
+	return (add_node_end(&(info->alias), str, 0) == NULL);
+}
+
+/**
+ * print_alias - prints an alias string
+ * @node: the alias node
+ *
+ * Return: Always 0 on success, 1 on error
+ */
+int print_alias(list_x *node)
+{
+	char *s = NULL, *b = NULL;
+
+	if (node)
 	{
-		perror("Error");
+		q = _strchr(node->str, '=');
+		for (b = node->str; b <= q; b++)
+			_putchar(*b);
+		_putchar('\'');
+		_puts(q + 1);
+		_puts("'\n");
 		return (0);
 	}
-	while (d > 0)
-	{
-		d = read(doc, &z, 1);
-		w = write(STDOUT_FILENO, &z, d);
-		if (w < 0)
-		{
-			return (-1);
-		}
-	}
-	_putchar('\n');
-	return (0);
-}
-/**
- * _echo_bul - echo command
- * Return: 0
- */
-int echo_bul(char **cmd, int st)
-{
-	char *path;
-	unsigned int  pid = getppid();
-
-	if (_strncmp(cmd[1], "$?", 2) == 0)
-	{
-		print_number_in(st);
-		PRINTER("\n");
-	}
-	else if (_strncmp(cmd[1], "$$", 2) == 0)
-	{
-		print_number(pid);
-		PRINTER("\n");
-
-	}
-	else if (_strncmp(cmd[1], "$PATH", 5) == 0)
-	{
-		path = _getenv("PATH");
-		PRINTER(path);
-		PRINTER("\n");
-		free(path);
-
-	}
-	else
-		return (print_echo(cmd));
-
 	return (1);
+}
+
+/**
+ * _myalias - mimics the alias builtin (man alias)
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ *  Return: Always 0
+ */
+int _myalias(info_t *info)
+{
+	int i = 0;
+	char *p = NULL;
+	list_t *node = NULL;
+
+	if (info->argc == 1)
+	{
+		node = info->alias;
+		while (node)
+		{
+			print_alias(node);
+			node = node->next;
+		}
+		return (0);
+	}
+	for (i = 1; info->argv[i]; i++)
+	{
+		p = _strchr(info->argv[i], '=');
+		if (p)
+			set_alias(info, info->argv[i]);
+		else
+			print_alias(node_starts_with(info->alias, info->argv[i], '='));
+	}
+
+	return (0);
 }
